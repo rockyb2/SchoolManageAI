9898d6b4ae4e4d8951731c24cc9968bb8e8af414
rockyb2 <davjonathan6@gmail.com>
2025-10-19 02:29:06 +0000

    Réintégré le dossier front comme dossier normal

---
+++ SchoolManageFront/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-3xl font-bold underline-transparent text-red-500">Hello world</h1>
+  <div>
+    <Icon icon="mdi:home" width="30" height="30" />
+    <Icon icon="mdi:account" width="30" height="30" color="blue" />
+  </div>
+
+</template>
+
+<script setup>
+
+import { Icon } from '@iconify/vue'
+</script>
